ca37def2a844b3fab905dbf5c0421c4e570ea306
mdethier <mdethier@laposte.net>
2023-02-20 16:53:54 +0100

    Debut footer Bootstrap

---
+++ footer.php
@@ -0,0 +1,36 @@
+<footer>
+   
+
+        <div class="container">
+            <div class="row">
+
+                <div class="col-3 p-2 contact">Contact</div>
+                <div class="col-3 p-2 mentionsLegales">Mentions Légales</div>
+                <div class="col-3 p-2 coordonnees">Coordonnées : 
+                    <div> 2 Rue des Arches 
+                        <br>77710 - Nanteau-sur-Lunain
+                    </div>
+                </div>
+                <div class="col-3 p-2 map"> <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5312.766994145201!2d2.8013632489946123!3d48.25699401281025!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47ef63b251d0f313%3A0x33b2f327186b7183!2s2%20Rue%20des%20Arches%2C%2077710%20Nanteau-sur-Lunain!5e0!3m2!1sfr!2sfr!4v1676905964933!5m2!1sfr!2sfr" width="300" height="150" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> 
+                </div>
+
+                
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <p class="text-center">®Copyright - Le W Company 2023</p>
+            </div>
+
+        </div>
+</footer>
+    
+
+    
+</body>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"> 
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+</html>
