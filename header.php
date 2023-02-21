0b6df6ffe0d262a746132e739869267a5ba96bd8
mdethier <mdethier@laposte.net>
2023-02-21 16:23:27 +0100

    Dossier assets & DB

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
       
         <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
-      </a>
+      
 
       <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
         <li><a href="#" class="nav-link px-2 link-secondary">Accueil</a></li>
--- lewcollection.sql
+++ lewcollection.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Feb 09, 2023 at 03:22 PM
+-- Generation Time: Feb 21, 2023 at 02:13 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -32,6 +32,15 @@ CREATE TABLE `category_items` (
   `name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `category_items`
+--
+
+INSERT INTO `category_items` (`id`, `name`) VALUES
+(1, 'piece'),
+(2, 'billet'),
+(3, 'timbre');
+
 -- --------------------------------------------------------
 
 --
@@ -46,6 +55,22 @@ CREATE TABLE `collections` (
   `id_category_item` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `collections`
+--
+
+INSERT INTO `collections` (`id`, `name`, `creation_date`, `id_user`, `id_category_item`) VALUES
+(1, -2147483648, '2023-02-14', 1, 1),
+(2, -2147483648, '2023-02-14', 1, 2),
+(3, -2147483648, '2023-02-14', 3, 3),
+(4, -2147483648, '2023-02-14', 4, 2),
+(5, -2147483648, '2023-02-14', 5, 1),
+(6, -2147483648, '2023-02-14', 6, 2),
+(7, -2147483648, '2023-02-14', 7, 2),
+(8, -2147483648, '2023-02-14', 8, 3),
+(9, -2147483648, '2023-02-14', 9, 1),
+(10, -2147483648, '2023-02-14', 10, 3);
+
 -- --------------------------------------------------------
 
 --
@@ -54,8 +79,22 @@ CREATE TABLE `collections` (
 
 CREATE TABLE `conditions` (
   `id` int NOT NULL,
-  `rating` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL
+  `rating` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `conditions`
+--
+
+INSERT INTO `conditions` (`id`, `rating`) VALUES
+(1, 'mint'),
+(2, 'near mint'),
+(3, 'very good plus'),
+(4, 'very good'),
+(5, 'good plus'),
+(6, 'good'),
+(7, 'fair'),
+(8, 'poor');
 
 -- --------------------------------------------------------
 
@@ -65,8 +104,24 @@ CREATE TABLE `conditions` (
 
 CREATE TABLE `countries` (
   `id` int NOT NULL,
-  `name` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL
+  `name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `countries`
+--
+
+INSERT INTO `countries` (`id`, `name`) VALUES
+(1, 'France'),
+(2, 'Portugal'),
+(3, 'Allemagne'),
+(4, 'Belgique'),
+(5, 'Espagne'),
+(6, 'Italie'),
+(7, 'Croatie'),
+(8, 'Autriche'),
+(9, 'Danemark'),
+(10, 'Angleterre');
 
 -- --------------------------------------------------------
 
@@ -76,7 +131,7 @@ CREATE TABLE `countries` (
 
 CREATE TABLE `images` (
   `id` int NOT NULL,
-  `url` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `url` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `id_item` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -96,9 +151,25 @@ CREATE TABLE `items` (
   `price` int NOT NULL,
   `actual_value` int NOT NULL,
   `creation_date` int NOT NULL,
-  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `obliteration` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`id_collection`, `id`, `name`, `added_date`, `id_condition`, `id_country`, `price`, `actual_value`, `creation_date`, `description`, `obliteration`) VALUES
+(1, 1, '', '2023-02-14', 1, 1, 1000, 1000, -2147483648, '', NULL),
+(1, 2, '', '2023-02-14', 2, 2, 1000, 1000, -2147483648, '', NULL),
+(1, 3, '', '2023-02-14', 3, 3, 1000, 1000, -2147483648, '', NULL),
+(1, 4, '', '2023-02-14', 4, 4, 1000, 1000, -2147483648, '', NULL),
+(1, 5, '', '2023-02-14', 5, 5, 1000, 1000, -2147483648, '', NULL),
+(1, 6, '', '2023-02-14', 6, 6, 1000, 1000, -2147483648, '', NULL),
+(1, 7, '', '2023-02-14', 7, 7, 1000, 1000, -2147483648, '', NULL),
+(1, 8, '', '2023-02-14', 8, 8, 1000, 1000, -2147483648, '', NULL),
+(1, 9, '', '2023-02-14', 1, 5, 1000, 1000, -2147483648, '', NULL),
+(1, 11, '', '2023-02-14', 7, 1, 1000, 1000, -2147483648, '', NULL);
 
 -- --------------------------------------------------------
 
@@ -113,6 +184,23 @@ CREATE TABLE `users` (
   `mail` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `is_admin` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `password`, `mail`, `is_admin`) VALUES
+(1, 'monkeydluffy', 'monkeyd', 'monkeydluffy@gmail.com', 0),
+(2, 'barbeblanche', 'blanchelabarbe', 'barbeblanche@gmail.com', 0),
+(3, 'ryu', 'streetfighter', 'ryu@gmail.com', 0),
+(4, 'sanji', 'lajambenoire', 'sanji@gmail.com', 0),
+(5, 'naruto', 'rasengan', 'naruto@gmail.com', 0),
+(6, 'kakashi', 'chidori', 'kakashi@gmail.com', 0),
+(7, 'kenshin', 'lameinverse', 'kenshin@gmail.com', 1),
+(8, 'goku', 'kamehameha', 'goku@gmail.com', 0),
+(9, 'vegeta', 'finalflash', 'vegeta@gmail.com', 0),
+(10, 'tenshinhan', 'kikoho', 'tenshinhan@gmail.com', 0),
+(11, 'chaozu', 'domdompa', 'chaozu@gmail.com', 0);
 
 --
 -- Indexes for dumped tables
@@ -174,25 +262,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `category_items`
 --
 ALTER TABLE `category_items`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `collections`
 --
 ALTER TABLE `collections`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `conditions`
 --
 ALTER TABLE `conditions`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `countries`
 --
 ALTER TABLE `countries`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `images`
@@ -204,13 +292,13 @@ ALTER TABLE `images`
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Constraints for dumped tables
