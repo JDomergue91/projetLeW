2dc163bf55a7a667b26e0c45e88fe978e92455f2
JDomergue91 <domerguejonathan91@gmail.com>
2023-02-23 14:02:44 +0100

    logo header

--- header.php
+++ header.php
@@ -8,6 +8,9 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" 
     integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/styles/allStyles.css">
+
+    
 
 </head>
 <body>
@@ -15,7 +18,7 @@
 
 <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
       
-        <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+       <img classe="img-header" src="./assets/images/timbre.png" alt=""> 
       
 
       <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
@@ -25,7 +28,8 @@
       </ul>
 
       <div class="col-md-3 text-end">
-        <button type="button" class="btn btn-outline-primary me-2">Connexion</button>
+        <button type="button" class="btn button-header me-2">Connexion</button>
+        <button type="button" class="btn button-header me-2">Profil</button>
       </div>
     </header>
     
