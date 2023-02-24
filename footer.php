f80f693a1de29d15b6fd8c11fd407560f17bf658
JDomergue91 <domerguejonathan91@gmail.com>
2023-02-24 11:51:56 +0100

    header footer done

--- footer.php
+++ footer.php
@@ -4,8 +4,8 @@
         <div class="container-fluid block elements">
             
 
-                <div class="col-2 centering"><a href="#"><span class="contact">Contact</span></a></div>
-                <div class="col-2 centering"><a href="#"><span class="legals">Mentions Légales</span></a></div>
+                <div class="col-2 centering"><a class="kill-underline" href="#"><span class="contact">Contact</span></a></div>
+                <div class="col-2 centering"><a class="kill-underline" href="#"><span class="legals">Mentions Légales</span></a></div>
                 <div class="col-2 centering">Coordonnées :
                         <br> 2 Rue des Arches 
                         <br>77710
--- pages/accueil.php
+++ pages/accueil.php
@@ -1,3 +1,15 @@
 <?php include './header.php'; ?>
-
+<h1>TEST</h1>
+<h1>TEST</h1>
+<h1>TEST</h1>
+<h1>TEST</h1>
+<h1>TEST</h1>
+<h1>TEST</h1>
+<h1>TEST</h1>
+<h1>TEST</h1>
+<h1>TEST</h1>
+<h1>TEST</h1>
+<h1>TEST</h1>
+<h1>TEST</h1>
+<h1>TEST</h1>
 <?php include './footer.php'; ?>
