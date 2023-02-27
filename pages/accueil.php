7ee4a7e2971f829f97cb70d7d27635ba72ff0a03
apenteado <NomUtilisateurGit>
2023-02-27 15:20:50 +0100

    degagerlesh1

--- pages/accueil.php
+++ pages/accueil.php
@@ -1,17 +1,5 @@
 <?php include './header.php'; ?>
-<h1>T</h1>
-<h1>TEST</h1>
-<h1>TEST</h1>
-<h1>TEST</h1>
-<h1>TEST</h1>
-<h1>TEST</h1>
-<h1>TEST</h1>
-<h1>TEST</h1>
-<h1>TEST</h1>
-<h1>TEST</h1>
-<h1>TEST</h1>
-<h1>TEST</h1>
-<h1>TEST</h1>
+
 
 <img src="phototimbre.jpg" alt="image page d'accueil" class ="imageAccueil">
 <p class = " texteAccueil "> Site web dédié aux collectionneurs qui souhaitent partager leurs collections de timbres, de billets et de pièces de monnaie.</p>
