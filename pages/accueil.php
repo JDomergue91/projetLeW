7b98b55a0080dfe5af179fb59cef69d421613907
apenteado <NomUtilisateurGit>
2023-02-27 15:19:24 +0100

    nomclassimg-et-modiftailleimg

--- pages/accueil.php
+++ pages/accueil.php
@@ -13,6 +13,6 @@
 <h1>TEST</h1>
 <h1>TEST</h1>
 
-<img src="phototimbre.jpg" alt="image page d'accueil">
+<img src="phototimbre.jpg" alt="image page d'accueil" class ="imageAccueil">
 <p class = " texteAccueil "> Site web dédié aux collectionneurs qui souhaitent partager leurs collections de timbres, de billets et de pièces de monnaie.</p>
 <?php include './footer.php'; ?>
