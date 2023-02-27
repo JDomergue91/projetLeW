2af706442070d1fc3a1dee64930225f4285074b2
apenteado <NomUtilisateurGit>
2023-02-27 16:17:44 +0100

    espaceImg

--- pages/accueil.php
+++ pages/accueil.php
@@ -2,5 +2,15 @@
 
 
 <img src="phototimbre.jpg" alt="image page d'accueil" class ="imageAccueil">
+</br>
+</br>
+</br>
+</br>
+</br>
 <p class = " texteAccueil "> Site web dédié aux collectionneurs qui souhaitent partager leurs collections de timbres, de billets et de pièces de monnaie.</p>
+</br>
+</br>
+</br>
+</br>
+</br>
 <?php include './footer.php'; ?>
