e53a22ec72dff7742047471b325587d6f41e529a
apenteado <NomUtilisateurGit>
2023-02-28 14:19:41 +0100

    correctionEnFrançais

--- pages/connexion.php
+++ pages/connexion.php
@@ -7,7 +7,7 @@
             <div class="card z-index-0 fadeIn3 fadeInBottom">
                 <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
                     <div class="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1">
-                        <h4 class="text-white font-weight-bolder text-center mt-2 mb-0">Sign in</h4>
+                        <h4 class="text-white font-weight-bolder text-center mt-2 mb-0">Se connecter</h4>
                         <div class="row mt-3">
                             <div class="col-2 text-center ms-auto">
                                 <a class="btn btn-link px-3" href="javascript:;">
@@ -39,15 +39,15 @@
                         <input type="email" class="form-control">
                     </div>
                     <div class="input-group input-group-outline mb-3">
-                        <label class="form-label">Password</label>
+                        <label class="form-label">mot de passe</label>
                         <input type="password" class="form-control">
                     </div>
                     <div class="form-check form-switch d-flex align-items-center mb-3">
-                        <input class="form-check-input" type="checkbox" id="rememberMe" checked=""><label class="form-check-label mb-0 ms-3" for="rememberMe">Remember me</label>
+                        <input class="form-check-input" type="checkbox" id="rememberMe" checked=""><label class="form-check-label mb-0 ms-3" for="rememberMe">Se souvenir de moi</label>
                     </div>
-                    <div class="text-center"><button type="button" class="btn bg-gradient-primary w-100 my-4 mb-2">Sign in</button>
+                    <div class="text-center"><button type="button" class="btn bg-gradient-primary w-100 my-4 mb-2">Se connecter</button>
                 </div>
-                <p class="mt-4 text-sm text-center">Don't have an account?<a href="../pages/sign-up.html" class="text-primary text-gradient font-weight-bold">Sign up
+                <p class="mt-4 text-sm text-center">Vous n'avez pas de compte?<a href="../pages/sign-up.html" class="text-primary text-gradient font-weight-bold">S'inscrire
 
                 </a>
             </p>
