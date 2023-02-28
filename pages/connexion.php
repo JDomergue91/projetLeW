9b3eaf641c62429a6bb87d5a2aee3f2af9a5b6c9
apenteado <NomUtilisateurGit>
2023-02-28 16:32:07 +0100

    mdpOublie

--- pages/connexion.php
+++ pages/connexion.php
@@ -66,24 +66,27 @@
     
 <main class="form-signin w-50 m-auto">
   <form>
-    <img class="mb-4" src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
-    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+    <h1 class="h3 mb-3 fw-normal">Connexion</h1>
 
     <div class="form-floating">
       <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-      <label for="floatingInput">Email address</label>
+      <label for="floatingInput">Adresse mail</label>
     </div>
     <div class="form-floating">
       <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-      <label for="floatingPassword">Password</label>
+      <label for="floatingPassword">Mot de passe</label>
     </div>
 
     <div class="checkbox mb-3">
       <label>
-        <input type="checkbox" value="remember-me"> Remember me
+        <input type="checkbox" value="remember-me"> Se souvenir de moi
       </label>
     </div>
-    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+    <button class="w-100 btn btn-lg btn-primary" type="submit">Se connecter</button>
+    <div class="mdpOublie">
+    </br>
+    Mot de passe oublié ? 
+    <a href="http://http://projet.lew"> Cliquez içi</a>
     <p class="mt-5 mb-3 text-muted">© 2017–2022</p>
   </form>
 </main>
